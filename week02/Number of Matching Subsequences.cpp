bd62f0ca81fbb6ab2a361aa9fd6b33f7260e18a2
Talat Tanbir <hossaintanbir0@gmail.com>
2022-04-21 14:20:01 +0600

    SOLUTION -> Number of Matching Subsequences

---
+++ week02/Number of Matching Subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<vector<int>> trackChar(26);
+        int result = 0;
+
+        for(int i=0; i<s.size(); i++)
+            trackChar[s[i]-'a'].push_back(i);
+
+        for(auto word: words){
+            int pos = -1;
+            bool isFound = true;
+
+            for(char ch: word){
+                auto it = upper_bound(trackChar[ch-'a'].begin(), trackChar[ch-'a'].end(), pos);
+                if(it == trackChar[ch-'a'].end())
+                    isFound = false;
+                else 
+                    pos = *it;
+            }
+
+            if(isFound)
+                result++;
+        }
+
+        return result;    
+    }
+};
