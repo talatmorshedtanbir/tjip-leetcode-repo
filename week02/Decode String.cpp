916293777c5cdf413ea8507190c7c8db7b1b1cd3
Talat Tanbir <hossaintanbir0@gmail.com>
2022-04-21 14:18:16 +0600

    SOLUTION -> Decode String

---
+++ week02/Decode String.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> counts;
+        stack<string> words;
+        string result = "";
+        int i = 0;
+
+        while(i < s.length()){
+            if(isdigit(s[i])){
+                int count = 0;
+                while(isdigit(s[i])){
+                    count = count*10 + (s[i] - '0');
+                    i++;
+                }
+                counts.push(count);
+            }
+            else if(s[i] == '['){
+                words.push(result);
+                result = "";
+                i++;
+            }
+            else if(s[i] == ']'){
+                string temp = words.top();
+                words.pop();
+                int count = counts.top();
+                counts.pop();
+                while(count--){
+                    temp += result;
+                }
+                result = temp;
+                i++;
+            }
+            else{
+                result += s[i];
+                i++;
+            }
+        }
+
+        return result;
+    }
+};
