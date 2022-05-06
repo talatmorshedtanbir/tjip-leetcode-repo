9c33809f68907ca0669d6ed6c72a9c23360625a2
Talat Tanbir <hossaintanbir0@gmail.com>
2022-05-06 23:55:05 +0600

    SOLUTION -> 454. 4Sum II

---
+++ week03/454. 4Sum II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int>  twoSum;
+        for(auto num1 : nums1) {
+            for(auto num2 : nums2) {
+                ++twoSum[num1+num2];
+            }
+        }
+        
+        int count = 0;
+        for(auto num3 : nums3) {
+            for(auto num4 : nums4) {
+                auto it = twoSum.find(0 - (num3 + num4));
+                if(it != twoSum.end()) {
+                    count += it->second;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
