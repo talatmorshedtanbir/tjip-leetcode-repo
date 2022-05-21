3fd7e7a271be499c6b632c301418c539f3a4700f
Talat Tanbir <hossaintanbir0@gmail.com>
2022-05-21 23:14:58 +0600

    SOLUTION -> Implement Trie (Prefix Tree)

---
+++ week04/208. Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,81 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+// Time Complexity: O(N*M) 
+// Spcae Complexity: O(N*M)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class TrieNode
+{
+public:
+    TrieNode *next[26];
+    bool is_word;
+    
+    TrieNode(bool b = false)
+    {
+        memset(next, 0, sizeof(next));
+        is_word = b;
+    }
+};
+
+class Trie
+{
+public:
+    Trie() {
+        root = new TrieNode;
+    }
+
+    ~Trie(){
+        clear(root);
+    }
+
+    void insert(string word) {
+        TrieNode *temp = root;
+
+        for(int i = 0; i < word.size(); i++) {
+            if(temp->next[word[i] - 'a'] == nullptr)
+                temp->next[word[i] - 'a'] = new TrieNode;
+            temp = temp->next[word[i]-'a'];
+        }
+
+        temp->is_word = true;
+    }
+
+    bool search(string word) {
+        TrieNode *temp = find(word);
+        if(temp != nullptr && (temp->is_word))
+            return true;
+        else
+            return false;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode *temp = find(prefix);
+        if(temp != nullptr)
+            return true;
+        else
+            return false;
+    }
+
+private:
+    TrieNode *root;
+
+    TrieNode* find(string word) {
+        TrieNode *p = root;
+        for(int i=0; i< word.size() && p!= NULL; ++i) {
+            p = p -> next[word[i]-'a'];
+        }
+
+        return p;
+    }
+
+    void clear(TrieNode *root){
+        for(int i = 0; i < 26; i++){
+            if(root->next[i] != nullptr){
+                clear(root->next[i]);
+            }
+        }
+
+        delete root;
+    }
+};
