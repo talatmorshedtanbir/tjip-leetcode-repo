eba10b5b122e8405b8756f989ade2fed805d153b
Talat Tanbir <hossaintanbir0@gmail.com>
2022-06-22 23:20:32 +0600

    SOLUTION -> 134. Gas Station

---
+++ week07/134. Gas Station.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/gas-station/
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int total_surplus = 0;
+        int surplus = 0;
+        int start = 0;
+        
+        for(int i = 0; i < n; i++){
+            total_surplus += gas[i] - cost[i];
+            surplus += gas[i] - cost[i];
+            
+            if(surplus < 0){
+                surplus = 0;
+                start = i + 1;
+            }
+        }
+        
+        return (total_surplus < 0) ? -1 : start;
+    }
+};
